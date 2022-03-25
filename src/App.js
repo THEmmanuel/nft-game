7cc9081e729d2b57bc391cb9544d74e0363af99c
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-03-25 20:34:24 +0100

    Check if wallet is connected

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
 
@@ -7,31 +7,46 @@ const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
-  return (
-    <div className="App">
-      <div className="container">
-        <div className="header-container">
-          <p className="header gradient-text">⚔️ Metaverse Slayer ⚔️</p>
-          <p className="sub-text">Team up to protect the Metaverse!</p>
-          <div className="connect-wallet-container">
-            <img
-              src="https://64.media.tumblr.com/tumblr_mbia5vdmRd1r1mkubo1_500.gifv"
-              alt="Monty Python Gif"
-            />
-          </div>
-        </div>
-        <div className="footer-container">
-          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-          <a
-            className="footer-text"
-            href={TWITTER_LINK}
-            target="_blank"
-            rel="noreferrer"
-          >{`built with @${TWITTER_HANDLE}`}</a>
-        </div>
-      </div>
-    </div>
-  );
+	const checkIfWalletIsConnected = () => {
+		const { ethereum } = window;
+
+		if (!ethereum) {
+			console.log('Make sure you have Metamask');
+			return;
+		} else {
+			console.log('We have the ethereum object', ethereum);
+		}
+	};
+
+	useEffect(() => {
+		checkIfWalletIsConnected();
+	}, []);
+
+	return (
+		<div className="App">
+			<div className="container">
+				<div className="header-container">
+					<p className="header gradient-text">⚔️ Metaverse Slayer ⚔️</p>
+					<p className="sub-text">Team up to protect the Metaverse!</p>
+					<div className="connect-wallet-container">
+						<img
+							src="https://64.media.tumblr.com/tumblr_mbia5vdmRd1r1mkubo1_500.gifv"
+							alt="Monty Python Gif"
+						/>
+					</div>
+				</div>
+				<div className="footer-container">
+					<img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+					<a
+						className="footer-text"
+						href={TWITTER_LINK}
+						target="_blank"
+						rel="noreferrer"
+					>{`built with @${TWITTER_HANDLE}`}</a>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default App;
