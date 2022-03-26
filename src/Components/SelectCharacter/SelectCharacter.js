ffdafe618d4c9a7a6c173563adb64734936f7c4d
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-03-26 19:21:06 +0100

    Add useEffect

--- src/Components/SelectCharacter/SelectCharacter.js
+++ src/Components/SelectCharacter/SelectCharacter.js
@@ -8,6 +8,25 @@ const SelectCharacter = ({ setCharacterNFT }) => {
 	const [characters, setCharacters] = useState([]);
 	const [gameContract, setGameContract] = useState(null);
 
+	useEffect(() => {
+		const { ethereum } = window;
+
+		if (ethereum) {
+			const provider = new ethers.providers.Web3Provider(ethereum);
+			const signer = provider.getSigner();
+			const gameContract = new ethers.Contract(
+				CONTRACT_ADDRESS,
+				myEpicGame.abi,
+				signer
+			);
+			// Set gameContract in state.
+			setGameContract(gameContract);
+		} else {
+			console.log('Ethereum object not found')
+		}
+	}, [])
+
+
 	return (
 		<div className='select-character-container'>
 			<h2>Select and mint your Hero.</h2>
