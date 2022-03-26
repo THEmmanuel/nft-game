0addf377ae6ffc633ab82dfec68acdd049c3aa1a
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-03-26 19:10:47 +0100

    Set up state

--- src/Components/SelectCharacter/SelectCharacter.js
+++ src/Components/SelectCharacter/SelectCharacter.js
@@ -5,6 +5,9 @@ import { CONTRACT_ADDRESS, transformCharacterData } from '../../constants';
 import myEpicGame from '../../utils/MyEpicGame.json';
 
 const SelectCharacter = ({ setCharacterNFT }) => {
+	const [characters, setCharacters] = useState([]);
+	const [gameContract, setGameContract] = useState(null);
+
 	return (
 		<div className='select-character-container'>
 			<h2>Select and mint your Hero.</h2>
