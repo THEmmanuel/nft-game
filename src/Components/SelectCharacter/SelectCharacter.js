e68c8daddf8833b4e72a1e8f716c47c0789bfe67
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-03-26 18:38:09 +0100

    Add imports

--- src/Components/SelectCharacter/SelectCharacter.js
+++ src/Components/SelectCharacter/SelectCharacter.js
@@ -1,7 +1,10 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import './SelectCharacter.css';
+import { ethers } from 'ethers';
+import { CONTRACT_ADDRESS, transformCharacterData } from '../../constants';
+import myEpicGame from '../../utils/MyEpicGame.json';
 
-const SelectCharacter = ({setCharacterNFT}) => {
+const SelectCharacter = ({ setCharacterNFT }) => {
 	return (
 		<div className='select-character-container'>
 			<h2>Select and mint your Hero.</h2>
