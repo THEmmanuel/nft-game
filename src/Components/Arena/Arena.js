1143d28f07af136dd6852522253ea1729acc8262
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-03-27 01:35:53 +0100

    Add Arena

---
+++ src/Components/Arena/Arena.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react';
+import { ethers } from 'ethers';
+import { CONTRACT_ADDRESS, transformCharacterData } from '../../constants';
+import myEpicGame from '../../utils/MyEpicGame.json';
+import './Arena.css';
+
+const Arena = ({ characterNFT }) => {
+	
+}
+
+export default Arena;
