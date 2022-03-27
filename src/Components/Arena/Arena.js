91870db9ac9e8975c0fa9a55aadb82bee8d9a6c3
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-03-27 12:13:04 +0100

    Add useEffect

--- src/Components/Arena/Arena.js
+++ src/Components/Arena/Arena.js
@@ -5,7 +5,24 @@ import myEpicGame from '../../utils/MyEpicGame.json';
 import './Arena.css';
 
 const Arena = ({ characterNFT }) => {
-	
+	const [gameContract, setGameContract] = useState(null);
+
+	useEffect(() => {
+		const { ethereum } = window;
+
+		if (ethereum) {
+			const provider = new ethers.providers.Web3Provider(ethereum);
+			const signer = provider.getSigner();
+			const gameContract = new ethers.Contract(
+				CONTRACT_ADDRESS,
+				myEpicGame.abi,
+				signer
+			);
+			setGameContract(gameContract);
+		} else {
+			console.log('Ethereum object not found');
+		}
+	}, [])
 }
 
 export default Arena;
